8f218a00728c5db33ff00e69e81ef900821b994b
Aditya Jain <adityajain.jan07@gmail.com>
2026-03-17 08:01:30 +0530

    fix: implemented UC6 using static methods and loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,61 @@
+
+
 public class OOPSBannerApp {
+
+    // Method to generate pattern for letter 'O'
+    public static String[] getOPattern() {
+        return new String[]{
+                String.join("", "  *****  "),
+                String.join("", " **   ** "),
+                String.join("", "**     **"),
+                String.join("", "**     **"),
+                String.join("", "**     **"),
+                String.join("", " **   ** "),
+                String.join("", "  *****  ")
+        };
+    }
+
+    // Method to generate pattern for letter 'P'
+    public static String[] getPPattern() {
+        return new String[]{
+                String.join("", "*******  "),
+                String.join("", "**    ** "),
+                String.join("", "**    ** "),
+                String.join("", "*******  "),
+                String.join("", "**       "),
+                String.join("", "**       "),
+                String.join("", "**       ")
+        };
+    }
+
+    // Method to generate pattern for letter 'S'
+    public static String[] getSPattern() {
+        return new String[]{
+                String.join("", "  ****** "),
+                String.join("", " **    **"),
+                String.join("", "**       "),
+                String.join("", "  ****** "),
+                String.join("", "       **"),
+                String.join("", "**    ** "),
+                String.join("", " ******  ")
+        };
+    }
+
+    // Main method
     public static void main(String[] args) {
 
-        System.out.println(" ***     ***    *****   ***** ");
-        System.out.println("*   *   *   *   *    *  *     ");
-        System.out.println("*   *   *   *   *    *  *     ");
-        System.out.println("*   *   *   *   *****   ***** ");
-        System.out.println("*   *   *   *   *       *     ");
-        System.out.println("*   *   *   *   *       *     ");
-        System.out.println(" ***     ***    *       ***** ");
+        String[] oPattern = getOPattern();
+        String[] pPattern = getPPattern();
+        String[] sPattern = getSPattern();
 
+        // Loop to print OOPS banner
+        for (int i = 0; i < oPattern.length; i++) {
+            System.out.println(
+                    oPattern[i] + " " +
+                    oPattern[i] + " " +
+                    pPattern[i] + " " +
+                    sPattern[i]
+            );
+        }
     }
 }
